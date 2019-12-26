846fa4d5746bece710a10b0e5e4acc9da22f3b03
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2019-12-26 13:25:55 +0100

    Repeat 79 lesson

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -5,11 +5,13 @@ import {
   AfterViewInit,
   Component,
   DoCheck,
+  ElementRef,
   Input,
   OnChanges,
   OnDestroy,
   OnInit,
   SimpleChanges,
+  ViewChild,
   ViewEncapsulation
 } from '@angular/core';
 
@@ -30,6 +32,7 @@ export class ServerElementComponent implements OnInit,
 
   @Input('srvElement') element: { type: string, name: string, content: string };
   @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
 
   constructor() {
     console.log('Constructor called');
@@ -46,6 +49,7 @@ export class ServerElementComponent implements OnInit,
 
   ngOnInit() {
     console.log('ngOnInit called');
+    console.log('Text content:' + this.header.nativeElement.textContent);
   }
 
   ngAfterContentInit(): void {
@@ -58,6 +62,7 @@ export class ServerElementComponent implements OnInit,
 
   ngAfterViewInit(): void {
     console.log('ngAfterViewInit called');
+    console.log('Text content:' + this.header.nativeElement.textContent);
   }
 
   ngAfterViewChecked(): void {
